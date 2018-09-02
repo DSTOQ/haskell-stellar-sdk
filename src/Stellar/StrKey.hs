85fc538d10c7e4f9940fa10859c2329b3886efba
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-09-02 19:11:34 +0200

    Decode public key

--- src/Stellar/StrKey.hs
+++ src/Stellar/StrKey.hs
@@ -4,12 +4,12 @@ import qualified Crypto.Error              as CE
 import qualified Crypto.PubKey.Ed25519     as ED
 import qualified Data.Base32String.Default as B32
 import qualified Data.ByteString           as BS
-import           Data.List                 (tail)
+import           Data.Crc16                (crc16xmodem)
 import           Protolude
 
 
 data PublicKeyError
-  = EmptyInput
+  = InvalidInputLength
   | InvalidVersion Word8
   | InvalidChecksum
   | CryptoError CE.CryptoError
@@ -25,19 +25,23 @@ publicKey = getPublicKeyBytes >=> decodePublicKey
   -- https://github.com/stellar/java-stellar-sdk/blob/master
   -- /src/main/java/org/stellar/sdk/StrKey.java#L97
   getPublicKeyBytes :: Text -> Either PublicKeyError ByteString
-  getPublicKeyBytes t = do
-    (version, bytes) <- uncons (BS.unpack . B32.toBytes . B32.fromText $ t)
-                        & maybe (throwError EmptyInput) pure
+  getPublicKeyBytes text = do
+    let bytes = BS.unpack $ B32.toBytes $ B32.fromText text
+    unless (length bytes == 35) $ throwError InvalidInputLength
     let (payload, checksum) = splitAt (length bytes - 2) bytes
-        payloadChecksum pl = notImplemented
-    unless (version == 0x30) (throwError $ InvalidVersion version)
-    unless (checksum == payloadChecksum payload) (throwError InvalidChecksum)
-    pure $ BS.pack $ tail payload
+    unless (toWord16le checksum == crc16xmodem payload)
+      $ throwError InvalidChecksum
+    let (version, key) = uncons payload & fromMaybe (0, [])
+    unless (version == 0x30) $ throwError $ InvalidVersion version
+    pure $ BS.pack key
 
   decodePublicKey :: ByteString -> Either PublicKeyError ED.PublicKey
   decodePublicKey = CE.onCryptoFailure (throwError . CryptoError) pure
     . ED.publicKey
 
+  toWord16le :: [Word8] -> Word16
+  toWord16le [h, l] = fromIntegral h .|. fromIntegral l `shiftL` 8
+  toWord16le _      = 0
 
 secretKey :: ByteString -> Either () ED.SecretKey
 secretKey = notImplemented
