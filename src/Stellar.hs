07f5b7176efb19af4df0faf146b4634e02a76847
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-09-04 14:16:07 +0200

    Re-export Stellar.Key

--- src/Stellar.hs
+++ src/Stellar.hs
@@ -2,8 +2,10 @@ module Stellar
   ( module Stellar.Types
   , module Stellar.Lenses
   , module Stellar.Crypto
+  , module Stellar.Key
   ) where
 
 import           Stellar.Crypto
+import           Stellar.Key
 import           Stellar.Lenses
 import           Stellar.Types
---
+++ src/Stellar/Key.hs
@@ -0,0 +1,9 @@
+module Stellar.Key
+  ( module Stellar.Key.Parser
+  , module Stellar.Key.Printer
+  , module Stellar.Key.Version
+  ) where
+
+import           Stellar.Key.Parser
+import           Stellar.Key.Printer
+import           Stellar.Key.Version
