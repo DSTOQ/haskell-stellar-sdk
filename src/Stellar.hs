5caa5761bb93c7edb099059a81858e636f6408e1
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-09-07 15:14:56 +0200

    Don't export Lenses by default in order not to pollute a global ns, use qualified imports instead.

--- src/Stellar.hs
+++ src/Stellar.hs
@@ -1,11 +1,9 @@
 module Stellar
   ( module Stellar.Types
-  , module Stellar.Lenses
   , module Stellar.Crypto
   , module Stellar.Key
   ) where
 
 import           Stellar.Crypto
 import           Stellar.Key
-import           Stellar.Lenses
 import           Stellar.Types
